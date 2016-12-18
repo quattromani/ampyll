0c83b78e99e57023abacc2e64bbcf1cec279873d
quattromani <max@maxquattromani.com>
2016-12-18 11:37:47 -0700

    added baseurl path to search.json

--- js/scripts/search.js
+++ js/scripts/search.js
@@ -1,7 +1,7 @@
 SimpleJekyllSearch.init({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('results-container'),
-  json: '/search.json',
+  json: '/ampyll/search.json',
   searchResultTemplate: '<li><a href="{url}" title="{title}">{title}<\/a><\/li>',
   noResultsText: 'No results found',
   limit: 10,
